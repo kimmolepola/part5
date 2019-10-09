3658fc5406ff703a28351fc649ec887967910454
Kimmo Lepola <kimmo.lepola@gmail.com>
2019-10-09 19:34:32 +0300

    blog list tests 00

--- bloglist_frontend/src/components/Blog.js
+++ bloglist_frontend/src/components/Blog.js
@@ -47,9 +47,10 @@ const Blog = ({
   return (
     <div>
       <div tabIndex="0" role="button" style={hideWhenExpanded} onKeyDown={() => toggleExpanded()} onClick={() => toggleExpanded()}>
-        {blog.title} {blog.author}
+        {blog.title} {blog.author} jepsis
       </div>
       <div tabIndex="0" role="button" style={showWhenExpanded} onKeyDown={() => toggleExpanded()} onClick={() => toggleExpanded()}>
+        juujoo
         {blog.title} {blog.author}<br />
         <a href={blog.url}>{blog.url}</a><br />
         {blog.likes}<button type="button" onClick={() => handleLike()}>like</button> <br />
